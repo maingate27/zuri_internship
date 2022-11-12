c9eafc36586917f16e7584151e0903748fece5ec
muhammed muktar <9youngmoney@gmail.com>
2022-11-12 16:06:21 +0100

    re update
    added all js in file

--- js_6_7/script.js
+++ js_6_7/script.js
@@ -1,40 +1,23 @@
 
 
 
-let img = document.getElementById("image")
-let btnprev = document.getElementById("btnprev")
-let btnnext = document.getElementById("btnnext")
+
+
 let slideIndex=1
 
 showSlide(slideIndex)
-displayScrolBtn()
 
 function nextSlide(n){
     showSlide(slideIndex +=n)
-    displayScrolBtn()
+    
 }
 
 function currentSlide(n){
     showSlide(slideIndex =n)
-    displayScrolBtn()
-}
-
-function displayScrolBtn(){
     
-img.onmouseover = function(){
-    btnprev.style.display = "block"
-    btnnext.style.display = "block"
-   // alert('hover')
-
 }
 
-img.onmouseleave = function(){
-    btnprev.style.display = "none"
-    btnnext.style.display = "none"
 
-  // alert("leave")
-}
-}
 
 function showSlide(n){
     let i
@@ -52,4 +35,25 @@ function showSlide(n){
     }
 
     slides[slideIndex-1].style.display="block" 
+
 }
+
+
+function showbtn(){
+
+    //alert("hello")
+    btnnext=document.getElementById("btnnext")
+   btnprev=document.getElementById("btnprev")
+    btnprev.style.display = "block"
+    btnnext.style.display = "block"
+ 
+ }
+ 
+ function hidebtn(){
+     
+     btnnext=document.getElementById("btnnext")
+     btnprev=document.getElementById("btnprev")
+     btnprev.style.display = "none"
+     btnnext.style.display = "none"
+ }
+ 
